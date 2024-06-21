0c00755217bb87653a3d1fa359c0179f6701dbbc
kyh <kyh@tigensoft.co.kr>
2024-06-21 11:50:33 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 //섹션1 ~ 섹션4 - '엔티티 매핑' (객체와 테이블 매핑) 까지

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            em.remove(findMember);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
